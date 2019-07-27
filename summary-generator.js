64d297d7850ffe58393a8498ac920eebd5b962ee
DanHodges <dhdgs690@gmail.com>
2019-07-26 20:37:10 -0500

    fix file paths

--- summary-generator.js
+++ summary-generator.js
@@ -3,7 +3,7 @@ const {terms} = require("./terms")
 terms.map(term => ` * [${term}](${term}.md)`)
 
 const summary = 
-`# Table of contents\n* [Initial page](README.md)\n${terms.map(term => `* [${term}](${term}.md)`).join('\n')}
+`# Table of contents\n* [Initial page](README.md)\n${terms.map(term => `* [${term}](${term.replace(/(\s+)/g, '\\$1')}.md)`).join('\n')}
  `
   fs.writeFile(`SUMMARY.md`, summary, err => {
       if (err) {
